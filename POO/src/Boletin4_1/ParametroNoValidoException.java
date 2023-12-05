e155d3a0995f33687171deb7cfe3d6667cda7521
pabloisla <114646785+pabloisla@users.noreply.github.com>
2023-12-05 20:45:04 +0100

    Add files via upload

---
+++ POO/src/Boletin4_1/ParametroNoValidoException.java
@@ -0,0 +1,12 @@
+package Boletin4_1;
+
+public class ParametroNoValidoException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ParametroNoValidoException(String m) {
+		super(m);
+	}
+}
