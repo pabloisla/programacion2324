53209d11d4219784faa743772c948cc63e4ede0f
pabloisla16 <pgeycam966@g.educaand.es>
2024-01-14 23:16:31 +0100

    Subida

---
+++ Ejercicios_Repaso_Youtube/src/Arrays/Arreglos.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class Arreglos {
+    public static void main(String[] args) {
+
+        /* int[] numeros = new int[3];
+
+        numeros[0] = 7;
+        numeros[1] = 10;
+        numeros[2] = 13;
+        */
+
+        int[] numeros = {5,7,10,54};
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println(numeros[i]);
+        }
+    }
+}
